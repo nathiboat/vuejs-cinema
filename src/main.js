e7d296d337f3d33fc3be9bf9141b335242ab8b0d
nathi.taengnim <nathi.taengnim@gimo.co.uk>
2017-05-26 17:29:39 +0100

    update component

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import './style.scss';
+import Vue from 'vue'
+
+import genres from './util/genres';
+
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list':{
+      template: `<div id="movie-list">
+                    <div v-for="movie in movies" class="movie"> {{ movie.title }}</div>
+                </div>`,
+      data() {
+        return {
+          movies: [
+            {title: ' movie 1'},
+            {title: 'mo vie 2'},
+            {title: 'movie 3'}
+          ]
+        }
+      }
+    },
+    'movie-filter':{
+      data(){
+        return{
+          genres
+        }
+      },
+      template: `<div id="movie-filter">
+                   <h2>Filter results</h2>
+                    <div class="filter-group">
+                        <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>  
+                    </div>
+                </div>`,
+      components:{
+        'check-filter':{
+          data(){
+            return{
+              checked: false
+            }
+          },
+          props: ['title'],
+          template: `<div v-bind:class="{'check-filter' : true, active: checked}" v-on:click="checked = !checked">
+                        <span class="checkbox"></span>
+                           <span class="check-filter-title">{{ title }}</span>
+                     </div>`
+        }
+      }
+    }
+  },
+  data: {
+    msg: 'heelow word'
+  }
+});
